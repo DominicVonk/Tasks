062c637277021558764831d8e3ceceac787b3cea
Dominic Vonk <dominic@vonk.io>
2021-08-21 12:15:36 +0200

    Update

--- lib/static.ts
+++ lib/static.ts
@@ -9,7 +9,7 @@ export function serveStatic(router: Router) {
       const headers = new Headers(response.headers);
       // Set the appropriate content-type header value.
       headers.set("content-type", contentType || "text/plain; charset=utf-8");
-      headers.set("content-security-policy", "default-src *");
+      headers.set("content-security-policy", "default-src * 'unsafe-inline'");
       ctx.response.headers = headers;
     });
   };
